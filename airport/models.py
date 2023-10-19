8869dc766afc1970717a4fe940b76e153a6f2ef5
OlegatorLE <oleh.travin@gmail.com>
2023-10-19 14:19:33 +0300

    Added models for user app and airport app. Added empty urls for user app and airport app.

--- airport/models.py
+++ airport/models.py
@@ -1,3 +1,95 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Airport(models.Model):
+    name = models.CharField(max_length=255)
+    closest_big_city = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return f"{self.name} ({self.closest_big_city})"
+
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        Airport,
+        related_name='departure_routes',
+        on_delete=models.CASCADE
+    )
+    destination = models.ForeignKey(
+        Airport,
+        related_name='arrival_routes',
+        on_delete=models.CASCADE
+    )
+    distance = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.source} - {self.destination}. {self.distance}km."
+
+
+class AirplaneType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Airplane(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+    airplane_type = models.ForeignKey(AirplaneType, on_delete=models.CASCADE)
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.first_name + " " + self.last_name
+
+    @property
+    def full_name(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
+class Flight(models.Model):
+    route = models.ForeignKey(Route, on_delete=models.CASCADE)
+    airplane = models.ForeignKey(Airplane, on_delete=models.CASCADE)
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+    crew = models.ManyToManyField(Crew)
+
+    def __str__(self) -> str:
+        return f"{self.route} arrives at {self.arrival_time}"
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+
+    def __str__(self) -> str:
+        return str(self.created_at)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    flight = models.ForeignKey(Flight, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.flight} (row:{self.row}, seat:{self.seat}"
