4351f8677cbe19d7388a846a7c333a165196dee5
OlegatorLE <oleh.travin@gmail.com>
2023-10-20 11:18:28 +0300

    fixed by black

--- airport/views.py
+++ airport/views.py
@@ -61,13 +61,10 @@ class FlightViewSet(viewsets.ModelViewSet):
         queryset = self.queryset
         if self.action == "list":
             queryset = (
-                queryset
-                .select_related("airplane")
-                .annotate(
-                    tickets_available=F(
-                        "airplane__rows") * F(
-                        "airplane__seats_in_row") - Count(
-                        "tickets")
+                queryset.select_related("airplane").annotate(
+                    tickets_available=F("airplane__rows")
+                    * F("airplane__seats_in_row")
+                    - Count("tickets")
                 )
             ).order_by("id")
         return queryset
@@ -91,67 +88,89 @@ class TicketViewSet(viewsets.ModelViewSet):
 
 
 class CustomAPIRootView(APIView):
-
     def get(self, request, format=None):
         if request.user.is_authenticated:
-            return Response({
-                "Airport-API": {
-                "airports": reverse(
-                    "airport:airport-list", request=request, format=format
-                ),
-                "routes": reverse(
-                    "airport:route-list", request=request, format=format
-                ),
-                "airplane_types": reverse(
-                    "airport:airplanetype-list",
-                    request=request,
-                    format=format
-                ),
-                "airplanes": reverse(
-                    "airport:airplane-list", request=request, format=format
-                ),
-                "crews": reverse(
-                    "airport:crew-list", request=request, format=format
-                ),
-                "flights": reverse(
-                    "airport:flight-list", request=request, format=format
-                ),
-                "orders": reverse(
-                    "airport:order-list", request=request, format=format
-                ),
-                "tickets": reverse(
-                    "airport:ticket-list", request=request, format=format
-                ),},
-                "User-API": {
-                "user_register": reverse(
-                    "user:create", request=request, format=format
-                ),
-                "token_obtain_pair": reverse(
-                    "user:token_obtain_pair", request=request, format=format
-                ),
-                "token_refresh": reverse(
-                    "user:token_refresh", request=request, format=format
-                ),
-                "token_verify": reverse(
-                    "user:token_verify", request=request, format=format
-                ),
-                "manage_user": reverse(
-                    "user:manage", request=request, format=format
-                ),
+            return Response(
+                {
+                    "Airport-API": {
+                        "airports": reverse(
+                            "airport:airport-list",
+                            request=request,
+                            format=format,
+                        ),
+                        "routes": reverse(
+                            "airport:route-list",
+                            request=request,
+                            format=format,
+                        ),
+                        "airplane_types": reverse(
+                            "airport:airplanetype-list",
+                            request=request,
+                            format=format,
+                        ),
+                        "airplanes": reverse(
+                            "airport:airplane-list",
+                            request=request,
+                            format=format,
+                        ),
+                        "crews": reverse(
+                            "airport:crew-list", request=request, format=format
+                        ),
+                        "flights": reverse(
+                            "airport:flight-list",
+                            request=request,
+                            format=format,
+                        ),
+                        "orders": reverse(
+                            "airport:order-list",
+                            request=request,
+                            format=format,
+                        ),
+                        "tickets": reverse(
+                            "airport:ticket-list",
+                            request=request,
+                            format=format,
+                        ),
+                    },
+                    "User-API": {
+                        "user_register": reverse(
+                            "user:create", request=request, format=format
+                        ),
+                        "token_obtain_pair": reverse(
+                            "user:token_obtain_pair",
+                            request=request,
+                            format=format,
+                        ),
+                        "token_refresh": reverse(
+                            "user:token_refresh",
+                            request=request,
+                            format=format,
+                        ),
+                        "token_verify": reverse(
+                            "user:token_verify", request=request, format=format
+                        ),
+                        "manage_user": reverse(
+                            "user:manage", request=request, format=format
+                        ),
+                    },
                 }
-            })
+            )
         else:
-            return Response({
-                "user_register": reverse(
-                    "user:create", request=request, format=format
-                ),
-                "token_obtain_pair": reverse(
-                    "user:token_obtain_pair", request=request, format=format
-                ),
-                "token_refresh": reverse(
-                    "user:token_refresh", request=request, format=format
-                ),
-                "token_verify": reverse(
-                    "user:token_verify", request=request, format=format
-                ),
-            })
+            return Response(
+                {
+                    "user_register": reverse(
+                        "user:create", request=request, format=format
+                    ),
+                    "token_obtain_pair": reverse(
+                        "user:token_obtain_pair",
+                        request=request,
+                        format=format,
+                    ),
+                    "token_refresh": reverse(
+                        "user:token_refresh", request=request, format=format
+                    ),
+                    "token_verify": reverse(
+                        "user:token_verify", request=request, format=format
+                    ),
+                }
+            )
